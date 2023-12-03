35e46f86109aa5866e8bb22016033e40e53a4214
Lera <LeraSakantseva3@yandex.ru>
2023-12-03 21:19:12 +0500

    module8-task1

--- modules/render-thumnail.js
+++ modules/render-thumnail.js
@@ -6,7 +6,7 @@ const getThumbnailFromTemplate = (pictureInfo) => {
 
   picture.querySelector('.picture_img').src = pictureInfo.url;
   picture.querySelector('.picture_img').alt = pictureInfo.description;
-  picture.querySelector('.picture_comments').textContent = pictureInfo.comments.length;
+  picture.querySelector('.picture_comments').textContent = pictureInfo.comments.Length;
   picture.querySelector('.picture_likes').textContent = pictureInfo.likes;
 
   return picture;
